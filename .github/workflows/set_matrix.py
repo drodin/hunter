adfcfbb059f1428e94ae3c0d232870d56dbb4a7f
drodin <rodin.dmitry@gmail.com>
2020-08-24 09:41:47 +0700

    Github Actions can override matrix and scipts

--- .github/workflows/set_matrix.py
+++ .github/workflows/set_matrix.py
@@ -18,10 +18,17 @@ if projects:
   with open('.github/workflows/ci/matrix.json') as json_matrix:
     matrix = json.load(json_matrix)
     for project in projects:
-      for leg in matrix['include']:
-        project_leg = leg.copy()
-        project_leg['example'] = project
-        include.append(project_leg)
+      matrix_override = 'cmake/projects/' + project + '/ci/matrix.json';
+      if os.path.isfile(matrix_override):
+        with open(matrix_override) as json_matrix_override:
+          project_matrix = json.load(json_matrix_override)
+          for project_leg in project_matrix['include']:
+            include.append(project_leg)
+      else:
+        for leg in matrix['include']:
+          project_leg = leg.copy()
+          project_leg['example'] = project
+          include.append(project_leg)
     matrix['include'] = include
     print(json.dumps(matrix))
 else:
