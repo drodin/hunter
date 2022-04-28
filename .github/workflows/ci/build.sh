1e5bab944d1f7883ac50b9c4276af4c0c1e67719
drodin <rodin.dmitry@gmail.com>
2022-04-28 17:32:30 +0700

    Upate CI

--- .github/workflows/ci/build.sh
+++ .github/workflows/ci/build.sh
@@ -6,9 +6,9 @@ python -m pip install --upgrade pip
 python -m pip install requests gitpython
 
 # Install latest Polly toolchains and scripts
-wget https://github.com/cpp-pm/polly/archive/master.zip
-unzip master.zip
-POLLY_ROOT="`pwd`/polly-master"
+wget https://github.com/drodin/polly/archive/refs/heads/pr.new_toolchains.zip
+unzip pr.new_toolchains.zip
+POLLY_ROOT="`pwd`/polly-pr.new_toolchains"
 export PATH="${POLLY_ROOT}/bin:${PATH}"
 
 # Install dependencies (CMake, Android NDK)
@@ -24,14 +24,9 @@ export ANDROID_NDK_r15c="`pwd`/_ci/android-ndk-r15c"
 export ANDROID_NDK_r16b="`pwd`/_ci/android-ndk-r16b"
 export ANDROID_NDK_r17="`pwd`/_ci/android-ndk-r17"
 
-# Use Xcode 11.3.1 instead of default 11.6 (for ios-13-2 toolchain, no iOS 13.2 SDK in Xcode 11.6)
-if [[ "$TOOLCHAIN" =~ "ios-nocodesign-13-2" ]]; then
-    export DEVELOPER_DIR="/Applications/Xcode_11.3.1.app/Contents/Developer"
-fi
-
-# Use Xcode 12.2 instead of default 12.4 (no macOS 11.0 SDK in Xcode 12.4)
-if [[ "$TOOLCHAIN" =~ "osx-11-0" ]]; then
-    export DEVELOPER_DIR="/Applications/Xcode_12.2.app/Contents/Developer"
+# Use Xcode 13.4 for macOS 12.x and iOS 15.x toolchains
+if [[ "$TOOLCHAIN" =~ "osx-12" || "$TOOLCHAIN" =~ "ios-nocodesign-15" ]]; then
+    export DEVELOPER_DIR="/Applications/Xcode_13.4.app/Contents/Developer"
 fi
 
 # Run build script
