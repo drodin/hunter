a4e92d2fb4890d2dd39d31885b5266e839855df6
drodin <rodin.dmitry@gmail.com>
2022-09-15 11:17:47 +0700

    Update 'SDL_ttf' package to v2.20.1-p0

--- cmake/projects/SDL_ttf/hunter.cmake
+++ cmake/projects/SDL_ttf/hunter.cmake
@@ -3,6 +3,7 @@
 
 include(hunter_add_version)
 include(hunter_cacheable)
+include(hunter_cmake_args)
 include(hunter_download)
 include(hunter_pick_scheme)
 
@@ -38,6 +39,27 @@ hunter_add_version(
     SHA1
     051ef15f3037002bd26414af56529e3bbb89a044
 )
+
+hunter_add_version(
+    PACKAGE_NAME
+    SDL_ttf
+    VERSION
+    "2.20.1-p0"
+    URL
+    "https://github.com/drodin/SDL_ttf/archive/refs/heads/hunter-2.20.1.zip"
+    SHA1
+    6a7a302dcf96e8565312d306f110610c024dd081
+)
+
+if(HUNTER_SDL_ttf_VERSION VERSION_GREATER_EQUAL 2.20.1)
+hunter_cmake_args(
+    SDL_ttf
+    CMAKE_ARGS
+    SDL2TTF_SAMPLES=OFF
+    SDL2TTF_VENDORED=OFF
+    SDL2TTF_HARFBUZZ=ON
+)
+endif()
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(SDL_ttf)
