7d981a6823da6605a9802f70fe0c0867869d7226
drodin <rodin.dmitry@gmail.com>
2024-04-18 16:48:58 +0700

    Update package 'SFML' to v2.5.1-p2 fiv videomode implementation for macOS

--- cmake/projects/SFML/hunter.cmake
+++ cmake/projects/SFML/hunter.cmake
@@ -7,6 +7,28 @@ hunter_add_version(
     PACKAGE_NAME
     SFML
     VERSION
+    2.5.1-p2
+    URL
+    "https://github.com/drodin/SFML/archive/fcff37b.tar.gz"
+    SHA1
+    8d2a517b6dcc596717d5b921a35a39a0ca96e077
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    SFML
+    VERSION
+    2.5.1-gl4es-p2
+    URL
+    "https://github.com/drodin/SFML/archive/e15d37f.tar.gz"
+    SHA1
+    8b3c9695e58e2b3684aeb94b6fffac2e7deb1655
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    SFML
+    VERSION
     2.5.1-p1
     URL
     "https://github.com/drodin/SFML/archive/04e3629.tar.gz"
